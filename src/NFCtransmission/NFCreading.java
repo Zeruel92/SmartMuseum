10aa3def7c2a7388f4cda3c2258549807471fd06
pspgt <gabry.t92@gmail.com>
2016-01-15 14:38:28 +0100

    NFCreading ora riceve correttamente il token da NFC; Code Cleaning

--- src/NFCtransmission/NFCreading.java
+++ src/NFCtransmission/NFCreading.java
@@ -13,6 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import smartmuseumtable.Login;
 import org.nfctools.examples.llcp.LlcpService;
+import org.nfctools.examples.llcp.NDefListenerNuovo;
 import org.nfctools.ndef.NdefListener;
 import org.nfctools.ndef.Record;
 import org.nfctools.utils.LoggingNdefListener;
@@ -23,42 +24,25 @@ public class NFCreading {
     public NFCreading() {
         try {
             Runtime rt = Runtime.getRuntime();
-            //String[] commands = {"python","C:\\Users\\Claudia\\Desktop\\nfcpy-0.10.2\\examples\\beam.py","recv","print"};
-            //String[] commands = {"java", "-cp", "C:\\Users\\Gabriele\\Desktop\\SmartMuseo\\nfctools-nesciu.jar", "org.nfctools.examples.llcp.LlcpService"};
             Process proc;
-            LlcpService nfc=new LlcpService(null,null);
-           nfc.run();
-            
-            //proc = rt.exec(commands);
-  //          BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            
-                //BufferedReader stdError = new BufferedReader(new 
-            //InputStreamReader(proc.getErrorStream()));
-                // read the output from the command
-            // System.out.println("Here is the standard output of the command:\n");
-            String s = null;
-    /*        while (true) {
-               while ((s = stdInput.readLine()) != null) {
-                    //Thread.sleep(1000);   
-                    if (s.contains("iot")) {
-                        System.out.println(s);
-                        proc.destroy();
-                        stdInput.close();
-                        Thread.sleep(1000);
-                        proc = rt.exec(commands);
-                        stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-                    }//fine if
+            LlcpService nfc = new LlcpService(null, null);
+            Thread t = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException ex) {
+                        }
+                        if (NDefListenerNuovo.getIstance().hasToken()) {
+                            String token = NDefListenerNuovo.getIstance().getRecord();
+                        }
+                    }
                 }
-
-            }*/
-                //Thread.sleep(1000);
-            //proc.destroy();
-
-// read any errors from the attempted command
-/*System.out.println("Here is the standard error of the command (if any):\n");
-             while ((s = stdError.readLine()) != null) {
-             System.out.println(s);
-             }*/
+            });
+            t.start();
+            nfc.run();
         } catch (Exception e) {
             e.printStackTrace();
         }
